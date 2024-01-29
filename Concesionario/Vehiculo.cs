d3b20906ef7b942536a1a6a408525dfb6c6cb317
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-01-29 12:37:12 +0100

    Clase Vehiculo Miembros privados, Propiedades y Constructores

--- Concesionario/Vehiculo.cs
+++ Concesionario/Vehiculo.cs
@@ -11,10 +11,42 @@ namespace Concesionario
       // Constantes
 
       // Miembros privados
+      private string _marca;
+      private string _modelo;
 
       // Constructores
+      public Vehiculo(string marca, string modelo) 
+      { 
+         Marca = marca;
+         Modelo = modelo;
+      }
 
       // Propiedades
+      public string Marca
+      {
+         get
+         {
+            return _marca;
+         }
+         set
+         {
+            // TODO: Proporcionar seguridad y proteccion al miembro - _marca
+            _marca = value;
+         }
+      }
+
+      public string Modelo
+      {
+         get 
+         { 
+            return _modelo;
+         }
+         set 
+         {
+            // TODO: Proporcionar seguridad y proteccion al miembro - _modelo
+            _modelo = value; 
+         }
+      }
 
       // Metodos publicos
 
