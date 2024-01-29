907c09946374f835d444b6aae4473bd93167d9a8
Roberto De Las Morenas Garcia <robertodelasmorenas2005@gmail.com>
2024-01-29 12:04:58 +0100

    Agregar archivos de proyecto.

---
+++ Concesionario/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Concesionario
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         // Controlador principal de la aplicacion
+
+         // Menu de acciones
+
+         // Procesamiento de las acciones
+
+      }
+   }
+}
